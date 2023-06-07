19abc31fb48f9e6d7488e89a116f0344fcc667bf
javiibm <javiera.jibm@gmail.com>
2023-06-07 18:42:53 -0400

    migrations y creacion de superuser

--- comunicados/core/models.py
+++ comunicados/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+NIVEL_CHOICES= [
+    ("GEN", "General"), 
+    ("PRE", "Ciclo Preescolar"), 
+    ("BAS", "Ciclo Básico"), 
+    ("MED", "Ciclo Medio")
+]
+
+class Categoria(models.Model):
+    nombre = models.TextField(max_length=20)
+    descripcion = models.TextField(max_length=40)
+
+class Comunicado(models.Model):
+    titulo = models.TextField(max_length=20)
+    detalle = models.TextField(max_length=40)
+    nivel = models.CharField(max_length=20, choices= NIVEL_CHOICES)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    fecha_envio = models.DateTimeField()
+    fecha_ultima_modificacion = models.DateTimeField()
+    publicado_por = models.ForeignKey(User, on_delete=models.CASCADE)
